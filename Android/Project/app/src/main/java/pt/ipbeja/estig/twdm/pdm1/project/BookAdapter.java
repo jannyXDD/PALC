a75fa69beda61884e2117ec0445dea8c09bd0e58
jannyXDD <joaobit@hotmail.com>
2021-04-21 11:58:27 +0100

    adicionar o projeto

---
+++ Android/Project/app/src/main/java/pt/ipbeja/estig/twdm/pdm1/project/BookAdapter.java
@@ -0,0 +1,79 @@
+package pt.ipbeja.estig.twdm.pdm1.project;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder>{
+    private static final String TAG = "BookAdapter";
+
+    private List<Book> bookList;
+    private Context context;
+
+    public BookAdapter(Context context, List<Book> bookList){
+        this.bookList = bookList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i(TAG, "onCreateViewHolder");
+        View view = LayoutInflater.from(this.context).inflate(R.layout.list_item, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+        Book book = this.bookList.get(position);
+        TextView textViewName = holder.getTextViewName();
+        textViewName.setText(book.getName());
+        TextView textViewDesc = holder.getTextViewDesc();
+        textViewDesc.setText(book.getDesc());
+        ImageView imageView = holder.getImageViewCover();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.bookList.size();
+    }
+
+
+    public class BookViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView imageViewCover;
+        private TextView textViewName;
+        private TextView textViewDesc;
+
+        public BookViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageViewCover = itemView.findViewById(R.id.imageView);
+            this.textViewName = itemView.findViewById(R.id.bookName);
+            this.textViewDesc = itemView.findViewById(R.id.bookDesc);
+        }
+
+        public ImageView getImageViewCover() {
+            return imageViewCover;
+        }
+
+        public TextView getTextViewName() {
+            return textViewName;
+        }
+
+        public TextView getTextViewDesc() {
+            return textViewDesc;
+        }
+    }
+}
---
+++ Android/Project/app/src/main/java/pt/ipbeja/estig/twdm/pdm1/project/MainActivity.java
@@ -0,0 +1,35 @@
+package pt.ipbeja.estig.twdm.pdm1.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        BookAdapter adapter = new BookAdapter(this, getBooks());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private static List<Book> getBooks(){
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("Cara Senhora Tosoni Os Postais de Casabella", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
+        bookList.add(new Book("Espectros do Cinema", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
+        bookList.add(new Book("Imitações da Vida", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
+        bookList.add(new Book("Obras-Primas da Arte Portuguesa", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
+
+        return bookList;
+    }
+}
