923addccf7042bb43dc745846f32c115c4d0a944
jannyXDD <joaobit@hotmail.com>
2021-04-21 14:45:02 +0100

    nada de especial

--- Android/Project/app/src/main/java/pt/ipbeja/estig/twdm/pdm1/project/MainActivity.java
+++ Android/Project/app/src/main/java/pt/ipbeja/estig/twdm/pdm1/project/MainActivity.java
@@ -25,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static List<Book> getBooks(){
         List<Book> bookList = new ArrayList<>();
-        bookList.add(new Book("Cara Senhora Tosoni Os Postais de Casabella", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "https://static.fnac-static.com/multimedia/Images/PT/NR/d4/46/68/6833876/1540-1.jpg"));
-        bookList.add(new Book("Espectros do Cinema", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
-        bookList.add(new Book("Imitações da Vida", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
-        bookList.add(new Book("Obras-Primas da Arte Portuguesa", "Livro por xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", null));
+        bookList.add(new Book("Cara Senhora Tosoni Os Postais de Casabella", "Descrição", "https://static.fnac-static.com/multimedia/Images/PT/NR/d4/46/68/6833876/1540-1.jpg"));
+        bookList.add(new Book("Espectros do Cinema", "Descrição", "https://img.wook.pt/images/espectros-do-cinema-jose-bertolo/MXwyNDU5MTQyMnwyMDcxMTA5MXwxNjEyMTM3NjAwMDAw/500x"));
+        bookList.add(new Book("Imitações da Vida", "Descrição", "https://img.wook.pt/images/imitacoes-da-vida-jose-bertolo/MXwyNDU5Mjk4OXwyMDcxNjU1NHwxNjEyMzk2ODAwMDAw/500x"));
+        bookList.add(new Book("Obras-Primas da Arte Portuguesa", "Descrição", "https://img.wook.pt/images/obras-primas-da-arte-portuguesa-delfim-sardo/MXwxMDk5Njc3MXw2NTY3NDYxfDEzODM1MjMyMDAwMDA=/500x"));
 
         return bookList;
     }
