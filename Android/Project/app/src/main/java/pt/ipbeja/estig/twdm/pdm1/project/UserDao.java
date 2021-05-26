cc484de403f5c04596836d569fe5a4ebc8246e31
jannyXDD <joaobit@hotmail.com>
2021-05-26 03:04:07 +0100

    starting the login

---
+++ Android/Project/app/src/main/java/pt/ipbeja/estig/twdm/pdm1/project/UserDao.java
@@ -0,0 +1,30 @@
+package pt.ipbeja.estig.twdm.pdm1.project;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM User")
+    List<User> getAll();
+
+    @Query("SELECT * FROM User WHERE id = :id")
+    User getById(long id);
+
+    @Insert
+    void add(User user);
+
+    @Insert
+    void add(List<User> users);
+
+    @Delete
+    void delete(User user);
+
+    @Update
+    void update(User user);
+}
